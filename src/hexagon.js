58ba20c4bf34a43baf5fe781e0c38251318203ba
Matthew J. Morrison <matt@ascentus.com>
2020-01-25 01:21:39 -0500

    vertex coloring

--- src/hexagon.js
+++ src/hexagon.js
@@ -1,10 +1,11 @@
 import {
     // BufferAttribute,
     // BufferGeometry,
-    // Color,
+    Color,
     Face3,
+    FaceColors,
     FontLoader,
-    // Geometry,
+    Geometry,
     Group,
     // Line,
     // LineBasicMaterial,
@@ -12,14 +13,15 @@ import {
     Math as ThreeMath,
     Mesh,
     MeshBasicMaterial,
+    // MeshStandardMaterial,
     // MeshPhongMaterial,
     // Shape,
     ShapeBufferGeometry,
     // ShapeGeometry,
     // Vector2,
+    VertexColors,
     Vector3,
-    WireframeGeometry,
-    Geometry
+    WireframeGeometry
 } from 'three';
 // import { scene } from '.';
 // import * as THREE from 'three';
@@ -72,29 +74,40 @@ function Hexagon(cX, cY, cZ) {
     geometry.vertices.push(new Vector3(corners[0].x * solidArea, 0, corners[0].z * solidArea));
     geometry.vertices.push(corners[0]);
 
+    const color = new Color('#' + Math.random().toString(16).slice(2, 8));
+    const color2 = new Color('#' + Math.random().toString(16).slice(2, 8));
+    const color3 = new Color('#' + Math.random().toString(16).slice(2, 8));
+
     for (let i = 0; i < corners.length; i++) {
         if (i > 0) {
             // solid area, border area
             geometry.vertices.push(new Vector3(corners[i].x * solidArea, 0, corners[i].z * solidArea));
             geometry.vertices.push(corners[i]);
 
-            geometry.faces.push(new Face3(0, i + i + 1, i + i - 1)); // (0, 7, 5)
-            geometry.faces.push(new Face3(i + i - 1, i + i + 1, i * 2)); // (5, 7, 6)
-            geometry.faces.push(new Face3(i * 2 + 1, i * 2 + 2, i * 2)); // (7, 8, 6)
+            geometry.faces.push(new Face3(0, i + i + 1, i + i - 1, null, [ color, color2, color3 ])); // (0, 7, 5)
+            geometry.faces.push(new Face3(i + i - 1, i + i + 1, i * 2, null, [ color, color2, color3 ])); // (5, 7, 6)
+            geometry.faces.push(new Face3(i * 2 + 1, i * 2 + 2, i * 2, null, [ color, color2, color3 ])); // (7, 8, 6)
         }
     }
 
     // end
-    geometry.faces.push(new Face3(0, 1, 11)); // 6
-    geometry.faces.push(new Face3(1, 12, 11));
-    geometry.faces.push(new Face3(1, 2, 12));
+    geometry.faces.push(new Face3(0, 1, 11, null, [ color, color2, color3 ])); // 6
+    geometry.faces.push(new Face3(1, 12, 11, null, [ color, color2, color3 ]));
+    geometry.faces.push(new Face3(1, 2, 12, null, [ color, color2, color3 ]));
 
+    geometry.computeFaceNormals();
     geometry.computeVertexNormals();
-    geometry.normalsNeedUpdate = true;
+    // geometry.normalsNeedUpdate = true;
     geometry.name = 'Hexagon';
     // geometry.rotateX(-90 * ThreeMath.DEG2RAD);
-    const color = '#' + Math.random().toString(16).slice(2, 8);
-    const material = new MeshBasicMaterial({ color: color });
+    // const color = '#' + Math.random().toString(16).slice(2, 8);
+    const material = new MeshBasicMaterial({
+        // vertexColors: VertexColors
+        vertexColors: FaceColors
+    });
+
+    // const material = new MeshBasicMaterial();
+    // const material = new MeshStandardMaterial({ color : color });
 
     // material.color = new THREE.Color('#ff0000');
     // material.needsUpdate = true;
@@ -105,6 +118,7 @@ function Hexagon(cX, cY, cZ) {
     // });
 
     const hexagon = new Mesh(geometry, material);
+    console.log(hexagon);
     hexagon.name = 'Hexagon';
     hexagon.position.x = this.position.x;
     // hexagon.position.y = Math.floor(Math.random() * 10);
